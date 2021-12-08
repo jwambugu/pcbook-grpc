d81a217b64a91038acccd46c4bf1a055cce8295a
Joram Wambugu <artisan254@gmail.com>
2021-12-08 15:20:28 +0300

    Provide the client credentials when connecting to the server

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"flag"
 	"fmt"
 	"github.com/jwambugu/pcbook-grpc/client"
 	"github.com/jwambugu/pcbook-grpc/factory"
 	"github.com/jwambugu/pcbook-grpc/protos/pb"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"io"
 	"log"
+	"os"
 	"strings"
 	"time"
 )
@@ -82,13 +87,43 @@ func authMethods() map[string]struct{} {
 	}
 }
 
+func loadTLSCredentials() (credentials.TransportCredentials, error) {
+	// Load certificate of the CA that signed the server's certificate
+	// Allows the client to verify authenticity the server's certificate
+	file, err := os.Open("certs/ca-cert.pem")
+	if err != nil {
+		return nil, fmt.Errorf("failed to open server CA pem: %v", err)
+	}
+
+	contents, err := io.ReadAll(file)
+	if err != nil && err != io.EOF {
+		return nil, fmt.Errorf("failed to read server CA pem: %v", err)
+	}
+
+	certPool := x509.NewCertPool()
+	if !certPool.AppendCertsFromPEM(contents) {
+		return nil, fmt.Errorf("failed to append server CA pem to cert pool")
+	}
+
+	config := &tls.Config{
+		RootCAs: certPool,
+	}
+
+	return credentials.NewTLS(config), nil
+}
+
 func main() {
 	serverAddress := flag.String("server-address", "0.0.0.0:8080", "the server address")
 	flag.Parse()
 
 	log.Printf("dialing server %s...", *serverAddress)
 
-	conn, err := grpc.Dial(*serverAddress, grpc.WithInsecure())
+	tlsCredentials, err := loadTLSCredentials()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	conn, err := grpc.Dial(*serverAddress, grpc.WithTransportCredentials(tlsCredentials))
 	if err != nil {
 		log.Fatalf("failed to dial server %s: %v", *serverAddress, err)
 	}
@@ -104,7 +139,7 @@ func main() {
 
 	cc, err := grpc.Dial(
 		*serverAddress,
-		grpc.WithInsecure(),
+		grpc.WithTransportCredentials(tlsCredentials),
 		grpc.WithUnaryInterceptor(interceptor.Unary()),
 		grpc.WithStreamInterceptor(interceptor.Stream()),
 	)
